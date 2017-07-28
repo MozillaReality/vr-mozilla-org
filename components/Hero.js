863004e72e6f7170ec4fce49649759b9bbadf5ea
Dan Brown <slightlyoffbeat@gmail.com>
2017-07-27 18:47:44 -0700

    brigade changes

--- components/Hero.js
+++ components/Hero.js
@@ -3,7 +3,7 @@ import Img from './Img';
 const Hero = () => (
   <div className="hero">
     <Img className="hero__icon mb2" src="/static/img/vr-icon.svg" alt="Virtual Reality" />
-    <h1 className="hero__text">VR for the Free and Open Web</h1>
+    <h1 className="hero__text">Virtual Reality for the Open Web</h1>
   </div>
 );
 
