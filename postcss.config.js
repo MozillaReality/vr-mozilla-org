d179df02bff68937a04e0885eeac16830a679f85
Dan Brown <slightlyoffbeat@gmail.com>
2017-07-31 20:46:59 -0700

    run uncss on build

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,28 @@
-const postcssEasyImport = require('postcss-easy-import');
-const autoprefixer = require('autoprefixer');
+// const postcssEasyImport = require('postcss-easy-import');
+// const autoprefixer = require('autoprefixer');
+// const uncss = require('postcss-uncss');
+//
+// module.exports = {
+//   plugins: [
+//     postcssEasyImport({ prefix: '_' }), // keep this first
+//     autoprefixer({ /* ...options */ }), // so imports are auto-prefixed too
+//     uncss({
+//       html: ['dist/index.html'],
+//       ignore: [],
+//     }),
+//   ],
+// };
 
-module.exports = {
-  plugins: [
-    postcssEasyImport({ prefix: '_' }), // keep this first
-    autoprefixer({ /* ...options */ }), // so imports are auto-prefixed too
-  ],
+const uncssOptions = {
+  html: ['dist/index.html'],
+  ignore: [],
+  ignoreSheets: ['/static/**/*'],
 };
+
+module.exports = ctx => ({
+  plugins: {
+    'postcss-easy-import': { prefix: '_' },
+    autoprefixer: {},
+    'postcss-uncss': ctx.env === 'production' ? uncssOptions : false,
+  },
+});
