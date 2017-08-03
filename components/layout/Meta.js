6dad47d5713c3edd47902f572f20ffb31f84e6cf
Dan Brown <slightlyoffbeat@gmail.com>
2017-08-03 13:03:01 -0700

    absolute URLS to relative URLs

--- components/layout/Meta.js
+++ components/layout/Meta.js
@@ -18,21 +18,21 @@ const Meta = ({ title, desc }) => (
       <meta property="og:url" content="https://vr.mozilla.org" />
       <meta property="og:title" content="Mozilla VR" />
       <meta property="og:description" content="VR for a free and open web" />
-      <meta property="og:image" content="/static/img/about/bg.png" />
+      <meta property="og:image" content="static/img/about/bg.png" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@mozilla" />
       <meta name="twitter:creator" content="@mozilla" />
       <meta name="twitter:title" content="Mozilla VR" />
       <meta name="twitter:description" content="VR for a free and open web" />
-      <meta name="twitter:image" content="/static/img/about/bg.png" />
-      <link rel="icon" type="image/png" href="/static/img/favicon16.png?s=16" sizes="16x16" />
-      <link rel="icon" type="image/png" href="/static/img/favicon32.png?s=32" sizes="32x32" />
-      <script async src="/static/js/dnt-helper.js" />
-      <script async src="/static/js/autotrack.js" />
+      <meta name="twitter:image" content="static/img/about/bg.png" />
+      <link rel="icon" type="image/png" href="static/img/favicon16.png?s=16" sizes="16x16" />
+      <link rel="icon" type="image/png" href="static/img/favicon32.png?s=32" sizes="32x32" />
+      <script async src="static/js/dnt-helper.js" />
+      <script async src="static/js/autotrack.js" />
       <script async src="https://www.google-analytics.com/analytics.js" />
-      <script async src="/static/js/ga.js" />
+      <script async src="static/js/ga.js" />
       <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" />
-      <script src="/static/js/webfont.js" />
+      <script src="static/js/webfont.js" />
     </Head>
   </div>
 );
