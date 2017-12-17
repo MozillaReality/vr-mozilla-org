8ebf91eec7d3b8e97f308455559e3e38c65ad3a6
J Smith <jscsmith@gmail.com>
2017-12-16 22:44:03 -0800

    update react scroll parallax
    - Updates `react-scroll-parallax` to `^1.1.1`
    - Wrap layout in <ParallaxProvider> to provide context
    - Remove `react-no-ssr` dependency, fixed SSR exporting issue
    - add image on load handler to update cached parallax values

--- components/Experience.js
+++ components/Experience.js
@@ -1,20 +1,18 @@
-// Since this is built with NextJS. Even though I'm using the static export
-// I need to 'support' SSR which is why the minor workaround for using window
-// and the NoSSR component
-
 import React from 'react';
-import NoSSR from 'react-no-ssr';
+import PropTypes from 'prop-types';
 import { Parallax } from 'react-scroll-parallax';
 import Img from './Img';
 
 class Experience extends React.Component {
+  static contextTypes = {
+    parallaxController: PropTypes.object.isRequired,
+  };
 
-  componentDidMount() {
-    const isBrowser = typeof window !== 'undefined';
-    // eslint-disable-next-line
-    const ParallaxController = isBrowser ? require('react-scroll-parallax').ParallaxController : undefined;
-    ParallaxController.init();
-  }
+  handleLoad = () => {
+    // updates cached values after image dimensions have loaded
+    const { parallaxController } = this.context;
+    parallaxController.update();
+  };
 
   render() {
     return (
@@ -25,18 +23,20 @@ class Experience extends React.Component {
         </p>
         <Img className="hidden-desktop mb4" src="/static/img/experience/devices-with-bg.png" alt="supported devices" />
         <div className="experience__devices">
-          <NoSSR>
-            <Parallax
-              className="hidden-mobile"
-              offsetYMax={8}
-              offsetYMin={-5}
-              offsetXMin={-5}
-              offsetXMax={2}
-              slowerScrollRate={false}
-            >
-              <Img src="/static/img/experience/devices-bg.png" alt="supported devices" />
-            </Parallax>
-          </NoSSR>
+          <Parallax
+            className="hidden-mobile"
+            offsetYMax={8}
+            offsetYMin={-5}
+            offsetXMin={-5}
+            offsetXMax={2}
+            slowerScrollRate={false}
+          >
+            <Img
+              onLoad={this.handleLoad}
+              src="/static/img/experience/devices-bg.png"
+              alt="supported devices"
+            />
+          </Parallax>
         </div>
 
         <div className="mt3">
--- components/Img.js
+++ components/Img.js
@@ -1,14 +1,20 @@
 import PropTypes from 'prop-types';
 import { prodUrl as baseUrl } from '../variables';
 
-const Img = ({ src, alt, className }) => (
-  <img className={className} src={`${baseUrl}${src}`} alt={alt} />
+const Img = ({ src, alt, className, onLoad }) => (
+  <img
+    onLoad={onLoad}
+    className={className}
+    src={`${baseUrl}${src}`}
+    alt={alt}
+  />
 );
 
 Img.propTypes = {
   src: PropTypes.string.isRequired,
   alt: PropTypes.string,
   className: PropTypes.string,
+  onLoad: PropTypes.func,
 };
 
 Img.defaultProps = {
