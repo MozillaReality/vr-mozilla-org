cb0da800ee0ea26ded112085135e849611f8318c
Dan Brown <slightlyoffbeat@gmail.com>
2018-09-17 23:22:37 -0700

    stop nesting links

--- src/components/FeaturedCard.js
+++ src/components/FeaturedCard.js
@@ -9,9 +9,9 @@ const FeaturedCard = () => (
     <div className="featured__ribbon">New</div>
     <img className="featured__logo mb2" alt="Firefox Reality" src={logo} />
     <p>A new browser for Virtual Reality.</p>
-    <Link className="featured__link" to="/firefox-reality">
+    <span className="featured__link" to="/firefox-reality">
       Learn More
-    </Link>
+    </span>
   </Link>
 );
 
